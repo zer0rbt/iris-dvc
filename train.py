f4e1368faf4397e1f90df35c7e89bddf88b5a78c
Fedor Shuvalov <fedorsh2004@gmail.com>
2024-12-21 10:55:22 +0300

    feat: v1.1 (RF)

--- train.py
+++ train.py
@@ -2,10 +2,15 @@ from sklearn.datasets import load_iris
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import json
 import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+matplotlib.use('Agg')
 
 data = load_iris()
 df = pd.DataFrame(data.data, columns=data.feature_names)
@@ -14,19 +19,36 @@ df['target'] = data.target
 df.to_csv('iris_data.csv', index=False)
 
 X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['target']), df['target'], test_size=0.3)
-model = RandomForestClassifier()
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
 accuracy = accuracy_score(y_test, y_pred)
-y_true = y_test
 metrics = {
-    "accuracy": accuracy_score(y_true, y_pred),
-    "precision": precision_score(y_true, y_pred, average='weighted'),
-    "recall": recall_score(y_true, y_pred, average='weighted'),
-    "f1_score": f1_score(y_true, y_pred, average='weighted')
+    "accuracy": accuracy,
+    "precision": precision_score(y_test, y_pred, average='weighted'),
+    "recall": recall_score(y_test, y_pred, average='weighted'),
+    "f1_score": f1_score(y_test, y_pred, average='weighted')
 }
-joblib.dump(model, 'model.pkl')
-# Сохранение метрик
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:")
+print(conf_matrix)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=data.target_names, yticklabels=data.target_names)
+plt.title("Confusion Matrix")
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+
+plt.savefig('confusion_matrix.png')
+plt.close()
+
+joblib.dump(model, 'random_forest_model.pkl')
+
+metrics["confusion_matrix"] = conf_matrix.tolist()
 with open('metrics.json', 'w') as f:
     json.dump(metrics, f)
