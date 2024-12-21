98d98b0acddbefe18f57453778aea5eb178cb319
Fedor Shuvalov <fedorsh2004@gmail.com>
2024-12-21 10:45:31 +0300

    feat: v2 (GB)

---
+++ gb_train.py
@@ -0,0 +1,54 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import json
+import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+
+matplotlib.use('Agg')
+data = load_iris()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+df['target'] = data.target
+
+df.to_csv('iris_data.csv', index=False)
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['target']), df['target'], test_size=0.3)
+
+model = GradientBoostingClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+metrics = {
+    "accuracy": accuracy,
+    "precision": precision_score(y_test, y_pred, average='weighted'),
+    "recall": recall_score(y_test, y_pred, average='weighted'),
+    "f1_score": f1_score(y_test, y_pred, average='weighted')
+}
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:")
+print(conf_matrix)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=data.target_names, yticklabels=data.target_names)
+plt.title("Confusion Matrix")
+plt.xlabel('Predicted Labels')
+plt.ylabel('True Labels')
+
+plt.savefig('confusion_matrix.png')
+plt.close()
+
+joblib.dump(model, 'gradient_boosting_model.pkl')
+
+metrics["confusion_matrix"] = conf_matrix.tolist()
+with open('metrics.json', 'w') as f:
+    json.dump(metrics, f)
